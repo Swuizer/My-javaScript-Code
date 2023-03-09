7d42343d12646358aba626096bc3e9773dbd096e
Swuizer <arghya143ghosh@gmail.com>
2023-03-09 23:57:38 +0530

    Initial commit

---
+++ Exercise-1/Problem_01.js
@@ -0,0 +1,9 @@
+// Write a function that takes two numbers as arguments and returns their sum.
+
+function sum(x, y){
+    add = x + y;
+    return add;
+}
+
+value = sum(5, 6);
+console.log(value);
---
+++ Exercise-1/Problem_02.js
@@ -0,0 +1,9 @@
+// Write a function that takes a string as an argument and returns its length.
+
+function string(str){
+    len = str.length;
+    return len;
+}
+
+value = string("Love Babbar");
+console.log(value);
